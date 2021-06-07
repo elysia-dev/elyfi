e2ad2893e0a2ad5e3d34ac7eaa693083df377fb7
donguk.seo <donguk.seo@modori.io>
2021-06-07 18:02:01 +0900

    Impl test-suites contract helper

---
+++ test-suites/utils/makeTestSuiteContracts.ts
@@ -0,0 +1,85 @@
+import {
+  makeInterestRateModel,
+  makeMoneyPool,
+  makeLToken,
+  makeUnderlyingAsset,
+  makeConnector,
+  makeTokenizer,
+  makeDataPipeline,
+  makeDToken,
+} from '../../test/utils/makeContract';
+import { defaultReserveData } from '../../test/utils/Interfaces';
+import { MockProvider } from '@ethereum-waffle/provider';
+import UserType from '../enums/UserType';
+import { ethers } from 'hardhat';
+import ElyfiContracts from '../types/ElyfiContracts';
+
+const makeTestSuiteContracts = async (provider: MockProvider): Promise<ElyfiContracts> => {
+  const wallets = provider.getWallets();
+  const deployer = wallets[UserType.Deployer];
+
+  const underlyingAsset = await makeUnderlyingAsset({
+    deployer,
+  });
+
+  const connector = await makeConnector({
+    deployer,
+  });
+
+  const moneyPool = await makeMoneyPool({
+    deployer,
+    connector,
+  });
+
+  const interestModel = await makeInterestRateModel({
+    deployer,
+  });
+
+  const lToken = await makeLToken({
+    deployer,
+    moneyPool,
+    underlyingAsset,
+  });
+
+  const dToken = await makeDToken({
+    deployer,
+    moneyPool,
+    underlyingAsset,
+  });
+
+  const tokenizer = await makeTokenizer({
+    deployer,
+    moneyPool,
+  });
+
+  const dataPipeline = await makeDataPipeline({
+    deployer,
+    moneyPool,
+  });
+
+  await moneyPool.addNewReserve(
+    underlyingAsset.address,
+    lToken.address,
+    dToken.address,
+    interestModel.address,
+    tokenizer.address,
+    defaultReserveData.moneyPoolFactor.toFixed()
+  );
+
+  [UserType.Account0, UserType.Account1, UserType.Account2, UserType.Account3].forEach(async (user) => {
+    await underlyingAsset.connect(deployer).transfer(wallets[user].address, ethers.utils.formatEther(1000));
+  })
+
+  return {
+    underlyingAsset,
+    connector,
+    moneyPool,
+    interestModel,
+    lToken,
+    dToken,
+    tokenizer,
+    dataPipeline,
+  }
+}
+
+export default makeTestSuiteContracts
