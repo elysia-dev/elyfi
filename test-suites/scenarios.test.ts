5a8b95d17901c42365231430c77bf87fb297a187
donguk.seo <donguk.seo@modori.io>
2021-06-07 18:03:47 +0900

    Test scenarios

--- test-suites/scenarios.test.ts
+++ test-suites/scenarios.test.ts
@@ -1,14 +1,35 @@
-import { expect } from "chai";
 import fs from "fs";
+import hre from 'hardhat';
+import ElyfiContracts from "./types/ElyfiContracts";
 import Scenario from "./types/scenario";
+import excuteStory from "./utils/excuteStory";
+import makeTestSuiteContracts from "./utils/makeTestSuiteContracts";
 
 fs.readdirSync('test-suites/scenarios').forEach((file) => {
   const scenario = require(`./scenarios/${file}`) as Scenario;
 
   describe(scenario.description, async () => {
+    const provider = hre.waffle.provider;
+    let elyfiContracts: ElyfiContracts;
+    let snapshotId: string;
+
+    before(async () => {
+      snapshotId = await provider.send('evm_snapshot', [])
+
+      elyfiContracts = await makeTestSuiteContracts(provider)
+    })
+
+    after(async () => {
+      await provider.send('evm_revert', [snapshotId])
+    })
+
     scenario.stories.forEach((story) => {
-      it(story.description, () => {
-        expect(story.expected).to.be.true
+      it(story.description, async () => {
+        await excuteStory(
+          story,
+          provider.getWallets()[story.actionMaker],
+          elyfiContracts,
+        )
       })
     })
   })
