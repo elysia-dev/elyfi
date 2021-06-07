b5a93d4e4b727769c41505af3773bcc548f69821
donguk.seo <donguk.seo@modori.io>
2021-06-07 19:01:42 +0900

    Refactor excuteStory

--- test-suites/utils/excuteStory.ts
+++ test-suites/utils/excuteStory.ts
@@ -13,84 +13,37 @@ import ActionType from "../enums/actinoType";
 import Story from "../types/story";
 import ElyfiContracts from "../types/ElyfiContracts";
 import takeDataSnapshot from "./takeDataSnapshot";
+import { ContractTransaction } from "@ethersproject/contracts";
+import { ReserveData, UserData } from "../../test/utils/Interfaces";
 require('../../test/assertions/equals')
 
-const excuteInvest = async (
+const excuteInvestor = async (
   account: Wallet,
   amount: BigNumber,
   elyfiContracts: ElyfiContracts,
-  expected: boolean,
+  doTransaction: () => Promise<ContractTransaction | undefined>,
+  calculateReserveData: (amount: BigNumber, reserveData: ReserveData, txTimestamp: BigNumber) => ReserveData,
+  calculateUserData: (amount: BigNumber, userData: UserData, reserveDateBefore: ReserveData, reserveDataAfter: ReserveData, txTimestamp: BigNumber) => UserData
 ) => {
-  await elyfiContracts.underlyingAsset.connect(account).approve(elyfiContracts.moneyPool.address, ethers.utils.parseEther('1000'));
-
   const [reserveDataBefore, userDataBefore] = await takeDataSnapshot(account, elyfiContracts)
 
-  let tx
-
-  try {
-    tx = await elyfiContracts.moneyPool
-      .connect(account)
-      .investMoneyPool(elyfiContracts.underlyingAsset.address, account.address, amount.toString());
-  } catch (e) {
-    expect(expected).to.be.false
-  }
-
-  expect(expected).to.be.true
+  const tx = await doTransaction();
 
   const [reserveDataAfter, userDataAfter] = await takeDataSnapshot(account, elyfiContracts);
 
-  const expectedReserveDataAfter = expectedReserveDataAfterInvestMoneyPool({
-    amountInvest: amount,
+  const expectedReserveDataAfter = calculateReserveData(
+    amount,
     reserveDataBefore,
-    txTimestamp: await getTimestamp(tx),
-  });
-  const expectedUserDataAfter = expectedUserDataAfterInvestMoneyPool({
-    amountInvest: amount,
-    userDataBefore: userDataBefore,
-    reserveDataBefore: reserveDataBefore,
-    reserveDataAfter: reserveDataAfter,
-    txTimestamp: await getTimestamp(tx),
-  });
+    await getTimestamp(tx),
+  );
 
-  expect(reserveDataAfter).to.be.equalReserveData(expectedReserveDataAfter);
-  expect(userDataAfter).to.be.equalUserData(expectedUserDataAfter);
-}
-
-const excuteWithdraw = async (
-  account: Wallet,
-  amount: BigNumber,
-  elyfiContracts: ElyfiContracts,
-  expected: boolean,
-) => {
-  const [reserveDataBefore, userDataBefore] = await takeDataSnapshot(account, elyfiContracts)
-
-  let tx
-
-  try {
-    tx = await elyfiContracts.moneyPool
-      .connect(account)
-      .withdrawMoneyPool(elyfiContracts.underlyingAsset.address, account.address, amount.toString());
-  } catch (e) {
-    expect(expected).to.be.false
-  }
-
-  expect(expected).to.be.true
-
-  const [reserveDataAfter, userDataAfter] = await takeDataSnapshot(account, elyfiContracts);
-
-  const expectedReserveDataAfter = expectedReserveDataAfterWithdrawMoneyPool({
-    amountWithdraw: amount,
-    reserveDataBefore: reserveDataBefore,
-    txTimestamp: await getTimestamp(tx),
-  });
-
-  const expectedUserDataAfter = expectedUserDataAfterWithdrawMoneyPool({
-    amountWithdraw: amount,
+  const expectedUserDataAfter = calculateUserData(
+    amount,
     userDataBefore,
     reserveDataBefore,
     reserveDataAfter,
-    txTimestamp: await getTimestamp(tx),
-  });
+    await getTimestamp(tx),
+  );
 
   expect(reserveDataAfter).to.be.equalReserveData(expectedReserveDataAfter);
   expect(userDataAfter).to.be.equalUserData(expectedUserDataAfter);
@@ -105,21 +58,81 @@ const excuteStory = async (
 
   switch (story.actionType) {
     case ActionType.investMoneyPool:
-      await excuteInvest(
+      await excuteInvestor(
         account,
         amount,
         elyfiContracts,
-        story.expected,
+        async () => {
+          try {
+            const tx = await elyfiContracts.moneyPool
+              .connect(account)
+              .withdrawMoneyPool(elyfiContracts.underlyingAsset.address, account.address, amount.toString());
+
+            expect(story.expected).to.be.true
+
+            return tx
+          } catch (e) {
+            expect(story.expected).to.be.false
+          }
+        },
+        (amountInvest, reserveDataBefore, txTimestamp) => {
+          return expectedReserveDataAfterInvestMoneyPool({
+            amountInvest,
+            reserveDataBefore,
+            txTimestamp,
+          });
+        },
+        (amountInvest, userDataBefore, reserveDataBefore, reserveDataAfter, txTimestamp) => {
+          return expectedUserDataAfterInvestMoneyPool({
+            amountInvest,
+            userDataBefore,
+            reserveDataBefore,
+            reserveDataAfter,
+            txTimestamp,
+          });
+        },
       )
       break;
+
     case ActionType.withdrawMoneyPool:
-      await excuteWithdraw(
+      await excuteInvestor(
         account,
         amount,
         elyfiContracts,
-        story.expected,
+        async () => {
+          try {
+            await elyfiContracts.underlyingAsset.connect(account).approve(elyfiContracts.moneyPool.address, ethers.utils.parseEther('1000'));
+
+            const tx = await elyfiContracts.moneyPool
+              .connect(account)
+              .investMoneyPool(elyfiContracts.underlyingAsset.address, account.address, amount.toString());
+
+            expect(story.expected).to.be.true
+
+            return tx
+          } catch (e) {
+            expect(story.expected).to.be.false
+          }
+        },
+        (amountWithdraw, reserveDataBefore, txTimestamp) => {
+          return expectedReserveDataAfterWithdrawMoneyPool({
+            amountWithdraw,
+            reserveDataBefore,
+            txTimestamp,
+          });
+        },
+        (amountWithdraw, userDataBefore, reserveDataBefore, reserveDataAfter, txTimestamp) => {
+          return expectedUserDataAfterWithdrawMoneyPool({
+            amountWithdraw,
+            userDataBefore,
+            reserveDataBefore,
+            reserveDataAfter,
+            txTimestamp,
+          });
+        },
       )
-      break
+      break;
+
     case ActionType.borrowAgainstABToken:
       break
     case ActionType.repayAgainstABToken:
