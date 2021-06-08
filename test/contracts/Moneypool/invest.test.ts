8df27683c320a6c3dfccf3970519dfb06989169e
donguk.seo <donguk.seo@modori.io>
2021-06-08 21:04:02 +0900

    Seperate invest & withdraw from moneypool test

---
+++ test/contracts/Moneypool/invest.test.ts
@@ -0,0 +1,58 @@
+import { BigNumber } from 'bignumber.js';
+import { waffle } from 'hardhat';
+import { expandToDecimals, getTimestamp, RAY } from '../../utils/Ethereum';
+import {
+  makeAllContracts,
+} from '../../utils/makeContract';
+import { expect } from 'chai';
+import {
+  expectedReserveDataAfterInvest,
+  expectedUserDataAfterInvest,
+} from '../../utils/Expect';
+import ElyfiContracts from '../../types/ElyfiContracts';
+import takeDataSnapshot from '../../utils/takeDataSnapshot';
+require('../../assertions/equals.ts');
+
+// TODO : Mockup user & reserve data
+describe('MoneyPool.invest', () => {
+  let elyfiContracts: ElyfiContracts
+
+  const provider = waffle.provider;
+  const [deployer, account1, account2] = provider.getWallets();
+
+  beforeEach(async () => {
+    elyfiContracts = await makeAllContracts(deployer)
+
+    await elyfiContracts.underlyingAsset.connect(deployer).transfer(account1.address, RAY);
+    await elyfiContracts.underlyingAsset.connect(deployer).transfer(account2.address, RAY);
+  });
+
+  it('update user data & reserve data', async () => {
+    const amountInvest = new BigNumber(expandToDecimals(10000, 18));
+    await elyfiContracts.underlyingAsset.connect(account1).approve(elyfiContracts.moneyPool.address, RAY);
+
+    const [reserveDataBefore, userDataBefore] = await takeDataSnapshot(account1, elyfiContracts)
+
+    const investTx = await elyfiContracts.moneyPool
+      .connect(account1)
+      .invest(elyfiContracts.underlyingAsset.address, account1.address, amountInvest.toFixed());
+
+    const [reserveDataAfter, userDataAfter] = await takeDataSnapshot(account1, elyfiContracts)
+
+    const expectedReserveData = expectedReserveDataAfterInvest({
+      amountInvest,
+      reserveDataBefore,
+      txTimestamp: await getTimestamp(investTx),
+    });
+    const expectedUserData = expectedUserDataAfterInvest({
+      amountInvest: amountInvest,
+      userDataBefore,
+      reserveDataBefore,
+      reserveDataAfter,
+      txTimestamp: await getTimestamp(investTx),
+    });
+
+    expect(reserveDataAfter).to.be.equalReserveData(expectedReserveData);
+    expect(userDataAfter).to.be.equalUserData(expectedUserData);
+  });
+});
---
+++ test/contracts/Moneypool/withdraw.test.ts
@@ -0,0 +1,69 @@
+import { BigNumber } from 'bignumber.js';
+import { ethers, waffle } from 'hardhat';
+import { getTimestamp } from '../../utils/Ethereum';
+import {
+  makeAllContracts,
+} from '../../utils/makeContract';
+import { expect } from 'chai';
+import {
+  expectedReserveDataAfterWithdraw,
+  expectedUserDataAfterWithdraw,
+} from '../../utils/Expect';
+import ElyfiContracts from '../../types/ElyfiContracts';
+import takeDataSnapshot from '../../utils/takeDataSnapshot';
+require('../../assertions/equals.ts');
+
+// TODO : Mockup user & reserve data
+describe('MoneyPool.withdraw', () => {
+  let elyfiContracts: ElyfiContracts
+
+  const provider = waffle.provider;
+  const [deployer, account1] = provider.getWallets();
+
+  beforeEach(async () => {
+    elyfiContracts = await makeAllContracts(deployer)
+
+    await elyfiContracts.underlyingAsset.connect(deployer).transfer(account1.address, ethers.utils.parseEther('5000'));
+  });
+
+  describe('when account invested', async () => {
+    describe('when account have enough reserve', async () => {
+      beforeEach(async () => {
+        await elyfiContracts.underlyingAsset.connect(account1).approve(elyfiContracts.moneyPool.address, ethers.utils.parseEther('20'));
+
+        await elyfiContracts.moneyPool
+          .connect(account1)
+          .invest(elyfiContracts.underlyingAsset.address, account1.address, ethers.utils.parseEther('10'));
+      })
+
+      // TODO : check account's testERC20 balance after withdrwal
+      it('withdraw the amount and upate user and reserve data', async () => {
+        const amountWithdraw = new BigNumber(ethers.utils.parseEther('5').toString());
+
+        const [reserveDataBefore, userDataBefore] = await takeDataSnapshot(account1, elyfiContracts)
+
+        const tx = await elyfiContracts.moneyPool
+          .connect(account1)
+          .withdraw(elyfiContracts.underlyingAsset.address, account1.address, ethers.utils.parseEther('5'));
+
+        const [reserveDataAfter, userDataAfter] = await takeDataSnapshot(account1, elyfiContracts)
+
+        const expectedReserveData = expectedReserveDataAfterWithdraw({
+          amountWithdraw,
+          reserveDataBefore,
+          txTimestamp: await getTimestamp(tx),
+        });
+        const expectedUserData = expectedUserDataAfterWithdraw({
+          amountWithdraw,
+          userDataBefore,
+          reserveDataBefore,
+          reserveDataAfter,
+          txTimestamp: await getTimestamp(tx),
+        });
+
+        expect(reserveDataAfter).to.be.equalReserveData(expectedReserveData);
+        expect(userDataAfter).to.be.equalUserData(expectedUserData);
+      });
+    })
+  })
+});
