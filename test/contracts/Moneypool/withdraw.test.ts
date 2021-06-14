e2b4a3f30975b672caded6dfcee82c8e2c9f034d
donguk.seo <donguk.seo@modori.io>
2021-06-14 11:20:02 +0900

    add withdrawal test cases

--- test/contracts/Moneypool/withdraw.test.ts
+++ test/contracts/Moneypool/withdraw.test.ts
@@ -1,4 +1,4 @@
-import { utils } from 'ethers'
+import { constants, utils } from 'ethers'
 import { waffle } from 'hardhat';
 import { getTimestamp } from '../../utils/Ethereum';
 import { expect } from 'chai';
@@ -12,39 +12,48 @@ import loadFixture from '../../utils/loadFixture';
 import utilizedMoneypool from '../../fixtures/utilizedMoneypool';
 require('../../assertions/equals.ts');
 
-// TODO : Mockup user & reserve data
 describe('MoneyPool.withdraw', () => {
   let elyfiContracts: ElyfiContracts
 
   const provider = waffle.provider;
   const [deployer, account1] = provider.getWallets();
+  const reserved = async () => {
+    return await elyfiContracts.underlyingAsset.balanceOf(elyfiContracts.lToken.address);
+  }
 
   beforeEach(async () => {
     const fixture = await loadFixture(utilizedMoneypool);
     elyfiContracts = fixture.elyfiContracts;
 
-    await elyfiContracts.underlyingAsset.connect(deployer).transfer(account1.address, utils.parseEther('5000'));
+    await elyfiContracts
+      .underlyingAsset
+      .connect(deployer)
+      .transfer(account1.address, utils.parseEther('5000'));
   });
 
-  describe('when account invested', async () => {
-    describe('when account have enough reserve', async () => {
-      beforeEach(async () => {
-        await elyfiContracts.underlyingAsset.connect(account1).approve(elyfiContracts.moneyPool.address, utils.parseEther('20'));
+  context('when a account invested', async () => {
+    beforeEach(async () => {
+      await elyfiContracts
+        .underlyingAsset
+        .connect(account1)
+        .approve(elyfiContracts.moneyPool.address, utils.parseEther('20'));
 
-        await elyfiContracts.moneyPool
-          .connect(account1)
-          .invest(elyfiContracts.underlyingAsset.address, account1.address, utils.parseEther('10'));
-      })
+      await elyfiContracts
+        .moneyPool
+        .connect(account1)
+        .invest(elyfiContracts.underlyingAsset.address, account1.address, utils.parseEther('10'));
+    })
 
-      // TODO : check account's testERC20 balance after withdrwal
+    context('when amount is valid', async () => {
       it('withdraw the amount and upate user and reserve data', async () => {
+        const reservedBefore = await reserved();
         const amountWithdraw = utils.parseEther('5');
 
         const [reserveDataBefore, userDataBefore] = await takeDataSnapshot(account1, elyfiContracts)
 
         const tx = await elyfiContracts.moneyPool
           .connect(account1)
-          .withdraw(elyfiContracts.underlyingAsset.address, account1.address, utils.parseEther('5'));
+          .withdraw(elyfiContracts.underlyingAsset.address, account1.address, amountWithdraw);
 
         const [reserveDataAfter, userDataAfter] = await takeDataSnapshot(account1, elyfiContracts)
 
@@ -62,7 +71,93 @@ describe('MoneyPool.withdraw', () => {
 
         expect(reserveDataAfter).to.equalReserveData(expectedReserveData);
         expect(userDataAfter).to.equalUserData(expectedUserData);
+
+        expect(await elyfiContracts.underlyingAsset.balanceOf(account1.address)).to.eq(utils.parseEther('4995'))
+
+        const reservedAfter = await reserved();
+        expect(reservedBefore.sub(reservedAfter)).to.eq(utils.parseEther('5'))
       });
+    })
+
+    context('when amount is uint max', async () => {
+      it('withdraw all', async () => {
+        const accountBalanceBefore = await elyfiContracts.underlyingAsset.balanceOf(account1.address);
+
+        await elyfiContracts
+          .moneyPool
+          .connect(account1)
+          .withdraw(
+            elyfiContracts.underlyingAsset.address,
+            account1.address,
+            constants.MaxUint256
+          );
+
+        expect(await elyfiContracts.lToken.balanceOf(account1.address)).to.eq(constants.Zero);
+        expect((await elyfiContracts.underlyingAsset.balanceOf(account1.address))
+          .sub(accountBalanceBefore))
+          .to.gte(utils.parseEther('5000'));
+      })
+    })
+
+    context('when moneypool is deactivated', async () => {
+      it('reverted ', async () => {
+        await elyfiContracts.connector
+          .connect(deployer)
+          .deactivateMoneyPool(elyfiContracts.moneyPool.address);
+
+        await expect(
+          elyfiContracts.moneyPool
+            .connect(account1)
+            .withdraw(
+              elyfiContracts.underlyingAsset.address,
+              account1.address,
+              utils.parseEther('2')
+            )
+        ).to.be.reverted;
+      });
+    });
+
+    context('when moneypool is paused', async () => {
+      it('reverted', async () => {
+        await expect(
+          elyfiContracts.moneyPool
+            .connect(account1)
+            .withdraw(
+              elyfiContracts.underlyingAsset.address,
+              account1.address,
+              utils.parseEther('2')
+            )
+        ).to.reverted;
+      });
+    });
+
+    context('when amount is bigger than max amount', async () => {
+      it('reverted', async () => {
+        await expect(
+          elyfiContracts
+            .moneyPool
+            .connect(account1)
+            .withdraw(
+              elyfiContracts.underlyingAsset.address,
+              account1.address,
+              utils.parseEther('11')
+            )
+        )
+      });
+    })
+  })
+
+  context('when a account does not invest', async () => {
+    it('reverted', async () => {
+      await expect(
+        elyfiContracts.moneyPool
+          .connect(account1)
+          .withdraw(
+            elyfiContracts.underlyingAsset.address,
+            account1.address,
+            utils.parseEther('1')
+          )
+      )
     })
   })
 });
