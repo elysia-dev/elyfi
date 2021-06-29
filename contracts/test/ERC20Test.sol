edd7c3d2171ef4971fe55368daf9917f37c687ba
jaebok <jaebok.lee@modori.io>
2021-06-29 16:25:22 +0900

    add faucet

--- contracts/test/ERC20Test.sol
+++ contracts/test/ERC20Test.sol
@@ -3,12 +3,32 @@ pragma solidity 0.8.4;
 
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
+/**
+ * @notice This ERC20 is only for the testnet.
+ */
 contract ERC20Test is ERC20 {
   constructor(
     uint256 totalSupply_,
     string memory name_,
     string memory symbol_
   ) ERC20(name_, symbol_) {
-    _mint(msg.sender, totalSupply_);
+    _mint(msg.sender, totalSupply_ / 2);
+    _owner = msg.sender;
+  }
+
+  address internal _owner;
+
+  /**
+   * @notice The faucet is for testing ELYFI functions
+   */
+  function faucet() external payable {
+    require(msg.value > 0, 'Not enough msg.value');
+
+    transfer(msg.sender, msg.value * 1000000);
+  }
+
+  function withdraw() external returns (bool) {
+    require(msg.sender == _owner, 'We will give back to the faucet');
+    return payable(address(msg.sender)).send(balanceOf(address(this)));
   }
 }
