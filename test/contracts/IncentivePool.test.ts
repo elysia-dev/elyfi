32190ac37717c6a4b19161cbeb856bcec75fa345
donguk.seo <donguk.seo@modori.io>
2021-07-11 22:07:48 +0900

    fix compile error

--- test/contracts/IncentivePool.test.ts
+++ test/contracts/IncentivePool.test.ts
@@ -1,7 +1,7 @@
 import { ethers, waffle } from 'hardhat';
 import ElyfiContracts from '../types/ElyfiContracts';
 import { BigNumber, utils } from 'ethers';
-require('../../assertions/equals.ts');
+require('../assertions/equals.ts');
 
 describe('', () => {
   let elyfiContracts: ElyfiContracts;
@@ -10,12 +10,12 @@ describe('', () => {
   const provider = waffle.provider;
   const [deployer, CSP, borrower, signer, liquidator] = provider.getWallets();
   context('deposit', async () => {
-    it('updates index and timestamp after deposit', async () => {});
+    it('updates index and timestamp after deposit', async () => { });
   });
   context('withdraw', async () => {
-    it('updates index and timestamp after withdraw', async () => {});
+    it('updates index and timestamp after withdraw', async () => { });
   });
   context('claimReward', async () => {
-    it('update userLastUpdateTimestamp and accured reward after claim reward', async () => {});
+    it('update userLastUpdateTimestamp and accured reward after claim reward', async () => { });
   });
 });
