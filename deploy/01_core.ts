96bb44164b5c3c64a4fbdb87aa8146a1d0847f26
copyNdpaste <julsnutini@naver.com>
2021-11-22 14:53:54 +0900

    Modify namedAccounts comment

--- deploy/01_core.ts
+++ deploy/01_core.ts
@@ -10,56 +10,56 @@ export enum ELYFIContractType {
   DATA_PIPELINE,
 }
 const deploy: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployer } = await hre.getNamedAccounts();
-  const { deploy } = hre.deployments;
+  // const { deployer } = await hre.getNamedAccounts();
+  // const { deploy } = hre.deployments;
 
-  const timeConverter = await deploy('TimeConverter', {
-    from: deployer,
-    log: true,
-  });
-  const index = await deploy('Index', {
-    from: deployer,
-    log: true,
-  });
-  const rate = await deploy('Rate', {
-    from: deployer,
-    log: true,
-  });
-  const validation = await deploy('Validation', {
-    from: deployer,
-    log: true,
-  });
-  const assetBond = await deploy('AssetBond', {
-    from: deployer,
-    log: true,
-    libraries: {
-      TimeConverter: timeConverter.address,
-    },
-  });
+  // const timeConverter = await deploy('TimeConverter', {
+  //   from: deployer,
+  //   log: true,
+  // });
+  // const index = await deploy('Index', {
+  //   from: deployer,
+  //   log: true,
+  // });
+  // const rate = await deploy('Rate', {
+  //   from: deployer,
+  //   log: true,
+  // });
+  // const validation = await deploy('Validation', {
+  //   from: deployer,
+  //   log: true,
+  // });
+  // const assetBond = await deploy('AssetBond', {
+  //   from: deployer,
+  //   log: true,
+  //   libraries: {
+  //     TimeConverter: timeConverter.address,
+  //   },
+  // });
 
-  const connector = await deploy('Connector', {
-    from: deployer,
-    log: true,
-  });
-  const moneyPool = await deploy('MoneyPool', {
-    from: deployer,
-    args: ['16', connector.address],
-    log: true,
-    libraries: {
-      AssetBond: assetBond.address,
-      Validation: validation.address,
-      TimeConverter: timeConverter.address,
-      Index: index.address,
-      Rate: rate.address,
-    },
-  });
-  const dataPipeline = await deploy('DataPipeline', {
-    from: deployer,
-    args: [moneyPool.address],
-    log: true,
-  });
+  // const connector = await deploy('Connector', {
+  //   from: deployer,
+  //   log: true,
+  // });
+  // const moneyPool = await deploy('MoneyPool', {
+  //   from: deployer,
+  //   args: ['16', connector.address],
+  //   log: true,
+  //   libraries: {
+  //     AssetBond: assetBond.address,
+  //     Validation: validation.address,
+  //     TimeConverter: timeConverter.address,
+  //     Index: index.address,
+  //     Rate: rate.address,
+  //   },
+  // });
+  // const dataPipeline = await deploy('DataPipeline', {
+  //   from: deployer,
+  //   args: [moneyPool.address],
+  //   log: true,
+  // });
 
-  console.log('core deploy done');
+  // console.log('core deploy done');
 };
 
 deploy.tags = ['core'];
